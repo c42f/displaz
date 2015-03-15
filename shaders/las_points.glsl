5371c1493d5817f8ede03a98cc40f00b4c9a698a
Chris Foster <chris42f@gmail.com>
2015-03-15 14:18:57 +1000

    Revert las shader to standard features
    Remove some nonstandard feature creep from the standard las points
    shader to keep it simple.

--- shaders/las_points.glsl
+++ shaders/las_points.glsl
@@ -12,7 +12,7 @@ uniform float pointRadius = 0.1;   //# uiname=Point Radius (m); min=0.001; max=1
 uniform float trimRadius = 1000000;//# uiname=Trim Radius; min=1; max=1000000
 uniform float exposure = 1.0;      //# uiname=Exposure; min=0.01; max=10000
 uniform float contrast = 1.0;      //# uiname=Contrast; min=0.01; max=10000
-uniform int colorMode = 0;         //# uiname=Colour Mode; enum=Intensity|Colour|Return Number|Number Of Returns|Point Source|Las Classification|Classified|File Number
+uniform int colorMode = 0;         //# uiname=Colour Mode; enum=Intensity|Colour|Return Number|Number Of Returns|Point Source|Las Classification|File Number
 uniform int selectionMode = 0;     //# uiname=Selection; enum=All|Classified|First Return|Last Return|First Of Several
 uniform float minPointSize = 0;
 uniform float maxPointSize = 400.0;
@@ -92,13 +92,6 @@ void main()
         else if (classification == 9) pointColor = vec3(0.0,  0.0,  0.8); // water
     }
     else if (colorMode == 6)
-    {
-        // Use intensity, but shade all points which have been classified green
-        pointColor = tonemap(intensity/400.0, exposure, contrast) * vec3(1);
-        if (classification != 0)
-            pointColor = 0.5*pointColor + vec3(0,0.75,0);
-    }
-    else if (colorMode == 7)
     {
         // Set point colour and marker shape cyclically based on file number to
         // give a unique combinations for 5*7 files.
