b99469bc99549e355dbf9096c8c3bb41ab332961
Chris Foster <chris.foster@roames.com.au>
2015-03-27 12:28:34 +1000

    Bugfix: Support per-vertex colors in mesh face shader
    The CPU code includes support for this, but somehow it's never been
    included in the shader.

--- shaders/meshface.glsl
+++ shaders/meshface.glsl
@@ -5,16 +5,17 @@ uniform mat4 projectionMatrix;
 uniform mat4 modelViewProjectionMatrix;
 
 #if defined(VERTEX_SHADER)
-uniform vec3 color = vec3(1.0);
-
 in vec3 position;
 in vec3 normal;
+in vec3 color;
 out vec3 position_eye;
+out vec3 fcolor;
 
 void main()
 {
     gl_Position = modelViewProjectionMatrix * vec4(position,1.0);
     position_eye = (modelViewMatrix * vec4(position,1.0)).xyz;
+    fcolor = color;
     //gl_FrontColor = vec4(color*abs(dot(normal, lightDir)), 1.0);
 }
 
@@ -23,6 +24,7 @@ void main()
 #elif defined(FRAGMENT_SHADER)
 uniform vec3 lightDir_eye = vec3(0.0,0.0,-1.0);
 in vec3 position_eye;
+in vec3 fcolor;
 
 out vec4 fragColor;
 
@@ -30,7 +32,7 @@ out vec4 fragColor;
 void main()
 {
     vec3 normal = normalize(cross(dFdx(position_eye), dFdy(position_eye)));
-    fragColor = vec4(vec3(abs(dot(normal, lightDir_eye))), 1);
+    fragColor = vec4(fcolor*abs(dot(normal, lightDir_eye)), 1);
 }
 #endif
 
