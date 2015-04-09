404e05926d37e7535df2ddbe748e3243db64b5d4
Chris Foster <chris.foster@roames.com.au>
2015-04-09 14:32:54 +1000

    Fix unix LD_LIBRARY_PATH in matlab dclf() binding

--- bindings/matlab/dclf.m
+++ bindings/matlab/dclf.m
@@ -1,3 +1,10 @@
 function dclf()
-    system('displaz -shader generic_points.glsl -clear');
+    fixLdLibPath = '';
+    if isunix()
+        % Reset LD_LIBRARY_PATH on unix - matlab adds to it to get the
+        % matlab version of libraries, which tend to conflict with the
+        % libraries used when building displaz.
+        fixLdLibPath = 'env LD_LIBRARY_PATH=""';
+    end
+    system(sprintf('%s displaz -shader generic_points.glsl -clear', fixLdLibPath));
 end
