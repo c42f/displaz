8e8ce8f210a5ae9e153cf5a2694a92551981066e
Chris Foster <chris42f@gmail.com>
2015-04-19 19:43:45 +1000

    Make the thirdparty/external build standalone
    Previously, LASlib and ilmbase were automatically downloaded and built
    as part of the main build.  Unfortunately this seems to cause more
    trouble than it's worth, including:
    
    - cmake's ExternalProject commands can't be used with the usual
      find_package() command since the project isn't compiled until build
      time.
    
    - LASlib is prone to change (including API breakage) without notice and
      doesn't have versioned downloads.  This just makes having it part of
      the main build error prone and embarrassing :-(
    
    Instead, convert thirdparty/external/CMakeLists.txt into a standalone
    build system which the user will just have to run by hand if they want
    these dependencies built for them.
    
    Also includes some minor tweaks to the variable naming in
    cmake/Find*.cmake for consistency with cmake conventions, and a new
    FindLASlib.cmake.

---
+++ cmake/FindLASlib.cmake
@@ -0,0 +1,27 @@
+# Find LASlib
+#
+# This sets:
+#   - LASLIB_FOUND:  system has LASlib
+#   - LASLIB_INCLUDE_DIRS: the LASlib include directories
+#   - LASLIB_LIBRARIES: the LASlib library
+#   - LASLIB_VERSION: the version string for LASlib
+
+find_path (LASLIB_INCLUDE_DIRS NAMES lasdefinitions.hpp)
+find_library (LASLIB_LIBRARY NAMES laslib)
+
+set (LASLIB_LIBRARIES ${LASLIB_LIBRARY})
+
+if (LASLIB_INCLUDE_DIRS)
+     file(STRINGS ${LASLIB_INCLUDE_DIRS}/lasdefinitions.hpp version_line
+          REGEX "#define *LAS_TOOLS_VERSION")
+     string(REGEX MATCH "[0-9]+" LASLIB_VERSION_STRING ${version_line})
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LASlib
+    FOUND_VAR LASLIB_FOUND
+    REQUIRED_VARS LASLIB_INCLUDE_DIRS LASLIB_LIBRARY
+    VERSION_VAR LASLIB_VERSION_STRING
+)
+
+mark_as_advanced(LASLIB_LIBRARY LASLIB_LIBRARIES LASLIB_INCLUDE_DIRS)
