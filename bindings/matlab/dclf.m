899199031713bb2acf72f05352ddcae230435742
Chris Foster <chris.foster@roames.com.au>
2015-05-15 11:14:57 +1000

    Fix dclf() binding to avoid blocking matlab

--- bindings/matlab/dclf.m
+++ bindings/matlab/dclf.m
@@ -6,5 +6,5 @@ function dclf()
         % libraries used when building displaz.
         fixLdLibPath = 'env LD_LIBRARY_PATH=""';
     end
-    system(sprintf('%s displaz -shader generic_points.glsl -clear', fixLdLibPath));
+    system(sprintf('%s displaz -shader generic_points.glsl -clear &', fixLdLibPath));
 end
