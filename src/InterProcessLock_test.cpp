14ddbc5b6347ce8121cbbab7bcf27079e9247ff4
Chris Foster <chris42f@gmail.com>
2015-07-12 12:17:30 +1000

    InterProcessLock tests without shell scripts
    Start lots of processes using QProcess rather than a shell script.  Damn
    you windows compatibility, a shell script is the right thing here.

--- src/InterProcessLock_test.cpp
+++ src/InterProcessLock_test.cpp
@@ -1,26 +1,71 @@
-#include <fstream>
+#include <memory>
+#include <vector>
+
+#include <QCoreApplication>
+#include <QProcess>
 
 #include "InterProcessLock.h"
 #include "util.h"
-
-void touch(const std::string& fileName)
-{
-    std::ofstream f(fileName);
-}
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
         return EXIT_FAILURE;
-    std::string fileName = argv[1];
-    InterProcessLock lk("InterProcessLock_test");
-    if (!lk.tryLock())
+    if (argv[1] == std::string("master"))
     {
-        touch(fileName + ".blocked");
+        QCoreApplication app(argc, argv);
+        QString prog = QCoreApplication::applicationFilePath();
+        std::vector<std::unique_ptr<QProcess>> procs;
+        QStringList args;
+        args << "slave";
+        // Start a whole bunch of processes.  Unfortunately QtProcess isn't
+        // robust enough to start more than about 100 at once on linux so
+        // stress testing more requires a different strategy.
+        const int nprocs = 100;
+        for (int i = 0; i < nprocs; ++i)
+        {
+            std::unique_ptr<QProcess> proc(new QProcess());
+            proc->start(prog, args);
+            procs.push_back(std::move(proc));
+        }
+        // Wait for them all to finish, accumulating exit status
+        int numAcquiredLocks = 0;
+        int numBlockedLocks = 0;
+        for (int i = 0; i < nprocs; ++i)
+        {
+            if (!procs[i]->waitForFinished() &&
+                procs[i]->exitStatus() != QProcess::NormalExit)
+            {
+                std::cerr << "Error in waitForFinished()\n";
+                return EXIT_FAILURE;
+            }
+            if (procs[i]->exitCode() == 0)
+                ++numAcquiredLocks;
+            else if (procs[i]->exitCode() == 1)
+                ++numBlockedLocks;
+        }
+        tfm::printfln("Acquired locks: %d", numAcquiredLocks);
+        tfm::printfln("Blocked locks: %d", numBlockedLocks);
+        if (numAcquiredLocks != 1)
+        {
+            std::cerr << "Exactly one process should have got the lock\n";
+            return EXIT_FAILURE;
+        }
+        if (numBlockedLocks != nprocs-1)
+        {
+            std::cerr << "Should have exactly N-1 blocked locks\n";
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        // Slave process.  This is what we're trying to test.
+        InterProcessLock lk("InterProcessLock_test");
+        if (!lk.tryLock())
+            return 1;
+        // We have the lock: sleep to let other processes exit
+        milliSleep(4000);
         return 0;
     }
-    // We have the lock: sleep to let other processes exit
-    touch(fileName + ".acquired");
-    milliSleep(10000);
-    return 0;
 }
