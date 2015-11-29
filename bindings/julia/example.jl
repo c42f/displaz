63bdf796074ca3fb2d4f992b7886e75c00df9b77
Chris Foster <chris42f@gmail.com>
2015-11-29 19:43:25 +1000

    Update julia bindings example

--- bindings/julia/example.jl
+++ bindings/julia/example.jl
@@ -1,16 +1,16 @@
 using Displaz
 
 function example(N)
-    t = linspace(0,1, N)
-    P = (10.+0.2*rand(size(t))).*[exp(5*t).*cos(100*pi*t)  exp(5*t).*sin(100*pi*t)  100*t];
-    C = [t (1 .- t) zeros(t)];
+    t = linspace(0,1, N)'
+    P = (10.+0.2*rand(size(t))) .* vcat(exp(5*t).*cos(100*pi*t),
+                                        exp(5*t).*sin(100*pi*t),
+                                        100*t);
+    C = vcat(t, 1.-t, zeros(t));
     sz  = map(Float32, t)
-    shape = mod(rand(Uint8, N), 6)
-    clf()
-    hold(true)
-    plot(P; color = C)
-    P[:,3] = -P[:,3]
-    plot(P; color = C, markersize = sz, markershape = shape)
+    shape = mod(rand(UInt8, N), 6)
+    plot3d(P, color = C, label="Top series")
+    P[3,:] = -P[3,:]
+    plot3d!(P, color = C, markersize = sz, markershape = shape, label="Bottom series")
 end
 
 example(1_000_000)
