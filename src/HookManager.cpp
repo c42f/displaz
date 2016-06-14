a5b9061d1ae2b601b729971cea0a1bd74a7b84e1
Jan <j.zill@uq.edu.au>
2016-06-14 14:57:30 +1000

    Hookable events from script

--- src/HookManager.cpp
+++ src/HookManager.cpp
@@ -5,12 +5,14 @@
 
 #include <QRegExp>
 
+#include "util.h"
 #include "HookFormatter.h"
+#include "IpcChannel.h"
 
 HookManager::HookManager(QObject* parent)
     : QObject(parent),
-    m_event(),
-    m_eventSpec(),
+    m_events(),
+    m_eventSpecs(),
     m_hooksSignedUp()
 {
 }
@@ -18,36 +20,48 @@ HookManager::HookManager(QObject* parent)
 
 void HookManager::connectHook(QByteArray eventSpec, HookFormatter* formatter)
 {
-    /// Remove keyword before key sequence, indicated by colon. At the moment,
-    /// everything up to (and including) the colon is removed and discarded, 
-    /// so not specifying 'key:' works as well.
-    QString specifier = QString::fromUtf8(eventSpec);
-    //specifier.count(QRegExp("[:]"));
-    specifier.replace(QRegExp(".*[:]"), "");
-    QKeySequence requested = QKeySequence::fromString(specifier, QKeySequence::PortableText);
+    /// Remove keyword before key sequence, indicated by colon, check if
+    /// valid specifier.
+    QStringList specifier =  QString::fromUtf8(eventSpec).split(":");
+
+    if(specifier.count() != 2)
+    {
+        dynamic_cast<IpcChannel*>(formatter->parent())->sendMessage(QByteArray("Error: Hook event specifier must be of the form 'key:x+y'.\n"));
+        delete formatter;
+        return;
+    }
+    else if(specifier[0] != "key")
+    {
+        dynamic_cast<IpcChannel*>(formatter->parent())->sendMessage(QByteArray("Error: Hook event specifier must be of the form 'key:x+y'.\n"));
+        delete formatter;
+        return;
+    }
+
+
+    QKeySequence requestedSpec = QKeySequence::fromString(specifier[1], QKeySequence::PortableText);
 
     /// Go through all shortcuts, see if one exists, if not create it and connect it to HookFormatter
-    int numberEvents = m_event.count();
+    int numberEvents = m_events.count();
     bool taken = false;
     for(int i=0; i<numberEvents; i++)
     {
-        if(m_eventSpec.at(i)==requested)
+        if(m_eventSpecs[i] == requestedSpec)
         {
             taken = true;
             m_hooksSignedUp[i]+=1;
             /// In case it was in use earlier and got disabled in deactivateEvent.
-            m_event.at(i)->setEnabled(true);
-            connect(m_event.at(i), SIGNAL(activated()), formatter, SLOT(hookActivated()));
+            m_events[i]->setEnabled(true);
+            connect(m_events[i], SIGNAL(activated()), formatter, SLOT(hookActivated()));
             formatter->setEventId(i);
         }
     }
 
     if(!taken)
     {
-        m_eventSpec.append(requested);
-        m_event.append(new QShortcut(m_eventSpec.at(numberEvents), qobject_cast<QWidget*>(this->parent())));
+        m_eventSpecs.append(requestedSpec);
+        m_events.append(new QShortcut(m_eventSpecs[numberEvents], qobject_cast<QWidget*>(this->parent())));
         m_hooksSignedUp.append(1);
-        connect(m_event.at(numberEvents), SIGNAL(activated()), formatter, SLOT(hookActivated()));
+        connect(m_events[numberEvents], SIGNAL(activated()), formatter, SLOT(hookActivated()));
         formatter->setEventId(numberEvents);
     }
 
@@ -57,8 +71,12 @@ void HookManager::connectHook(QByteArray eventSpec, HookFormatter* formatter)
 
 void HookManager::deactivateEvent(int eventId)
 {
-    m_hooksSignedUp[eventId]--;
-
-    if(m_hooksSignedUp.at(eventId) == 0)
-        m_event.at(eventId)->setEnabled(false);
+    if((eventId < m_hooksSignedUp.count()) && (eventId>=0))
+    {
+        m_hooksSignedUp[eventId]--;
+        if(m_hooksSignedUp[eventId] == 0)
+            m_events[eventId]->setEnabled(false);
+    }
+    else
+        std::cerr << "Error: Requested deletion of hook out of range.\n";
 }
