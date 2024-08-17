20bd8cd20582d28e32d378013e23d8218bc71970
Nigel Stewart <nigels@nigels.com>
2024-08-17 20:42:24 +1000

    Consolidate _glError implementation

--- src/render/gldebug.cpp
+++ src/render/gldebug.cpp
@@ -13,18 +13,21 @@ void _glError(const char *file, int line)
 
     while (err != GL_NO_ERROR)
     {
-        std::string error;
+        const std::map<GLenum, const char*> errorMap =
+            {
+                { GL_INVALID_OPERATION,             "GL_INVALID_OPERATION"             },
+                { GL_INVALID_ENUM,                  "GL_INVALID_ENUM"                  },
+                { GL_INVALID_VALUE,                 "GL_INVALID_VALUE"                 },
+                { GL_OUT_OF_MEMORY,                 "GL_OUT_OF_MEMORY"                 },
+                { GL_INVALID_FRAMEBUFFER_OPERATION, "GL_INVALID_FRAMEBUFFER_OPERATION" }
+            };
 
-        switch (err)
+        const auto errorIterator = errorMap.find(err);
+        if (errorIterator != errorMap.end())
         {
-            case GL_INVALID_OPERATION:      error="INVALID_OPERATION";      break;
-            case GL_INVALID_ENUM:           error="INVALID_ENUM";           break;
-            case GL_INVALID_VALUE:          error="INVALID_VALUE";          break;
-            case GL_OUT_OF_MEMORY:          error="OUT_OF_MEMORY";          break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:  error="INVALID_FRAMEBUFFER_OPERATION";  break;
+            tfm::printfln("%s - %s:%i", errorIterator->second, file, line);
         }
 
-        tfm::printfln("GL_%s - %s:%i", error, file, line);
         err = glGetError();
     }
 }
