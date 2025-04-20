17b74e663dabeef4fc291142949e9114c19090bf
Nigel Stewart <nigels@nigels.com>
2025-04-20 21:44:09 +1000

    Simplify recent file implementation

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -19,8 +19,6 @@
 #include "HookFormatter.h"
 #include "HookManager.h"
 
-#include <set>
-
 #include <QSignalMapper>
 #include <QThread>
 #include <QScreen>
@@ -698,6 +696,7 @@ void MainWindow::openFiles()
         QDir dir = QFileInfo(filename).dir();
         dir.makeAbsolute();
         m_settings.setValue("lastDirectory", dir.path());
+        m_recent.removeAll(filename);
         m_recent.append(filename);
         m_fileLoader->loadFile(FileLoadInfo(filename));
     }
@@ -712,6 +711,7 @@ void MainWindow::openRecent()
         QDir dir = QFileInfo(filename).dir();
         dir.makeAbsolute();
         m_settings.setValue("lastDirectory", dir.path());
+        m_recent.removeAll(filename);
         m_recent.append(filename);
         m_fileLoader->loadFile(FileLoadInfo(filename));
     }
@@ -977,28 +977,12 @@ void MainWindow::readSettings()
 
 void MainWindow::writeSettings()
 {
-    // De-duplicate and limit recents
-    QStringList recent;
-    std::set<QString> set;
-    for (auto i = m_recent.rbegin(); i != m_recent.rend(); ++i)
+    // Limit recents
+    while (m_recent.size() > m_recentLimit)
     {
-        if (i->isEmpty())
-        {
-            continue;
-        }
-
-        if (!set.count(*i))
-        {
-            set.insert(*i);
-            recent.prepend(*i);
-            if (recent.size() == m_recentLimit)
-            {
-                break;
-            }
-        }
+        m_recent.removeFirst();
     }
-
-    m_settings.setValue("recent", recent);
+    m_settings.setValue("recent", m_recent);
 
     m_settings.setValue("geometry", saveGeometry());
     m_settings.setValue("windowState", saveState());
