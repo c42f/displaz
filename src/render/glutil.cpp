e57752dd511c280c36518948bbb50f62bb6d581f
Nigel Stewart <nigels@nigels.com>
2025-04-30 07:18:19 +1000

    Move TransformState into it's own compilation unit

--- src/render/glutil.cpp
+++ src/render/glutil.cpp
@@ -6,69 +6,6 @@
 #include "tinyformat.h"
 
 //------------------------------------------------------------------------------
-/// Utility to handle transformation state
-void TransformState::setUniform(GLuint prog, const char* name, const M44d& mat)
-{
-    GLint loc = glGetUniformLocation(prog, name);
-    if (loc == -1)
-        return;
-    M44f M(mat);
-    glUniformMatrix4fv(loc, 1, GL_FALSE, &M[0][0]);
-}
-
-TransformState TransformState::translate(const Imath::V3d& offset) const
-{
-    TransformState res(*this);
-    res.modelViewMatrix =  M44d().setTranslation(offset) * modelViewMatrix;
-    return res;
-}
-
-TransformState TransformState::scale(const Imath::V3d& scalar) const
-{
-    TransformState res(*this);
-    res.modelViewMatrix =  M44d().setScale(scalar) * modelViewMatrix;
-    return res;
-}
-
-TransformState TransformState::rotate(const Imath::V4d& rotation) const
-{
-    TransformState res(*this);
-    res.modelViewMatrix =  M44d().rotate(V3d(rotation.x*rotation.w,rotation.y*rotation.w,rotation.z*rotation.w)) * modelViewMatrix;
-    return res;
-}
-
-
-void TransformState::setUniforms(GLuint prog) const
-{
-    // Note: The matrices must have a sensible representation in float32
-    // precision by the time they get here.
-    setUniform(prog, "projectionMatrix", projMatrix);
-    setUniform(prog, "modelViewMatrix", modelViewMatrix);
-    M44d mvproj = modelViewMatrix * projMatrix;
-    setUniform(prog, "modelViewProjectionMatrix", mvproj);
-}
-
-void TransformState::setOrthoProjection(double left, double right, double bottom, double top, double nearVal, double farVal)
-{
-    // Note: Create an orthographic matrix from input values
-    if ((left == right) || (bottom == top) || (nearVal == farVal))
-        throw DisplazError("Invalid input for orthographic projection.");
-
-    double xx = 2.0/(right-left);
-    double yy = 2.0/(top-bottom);
-    double zz = -2.0/(farVal-nearVal);
-
-    double tx = -(right+left)/(right-left);
-    double ty = -(top+bottom)/(top-bottom);
-    double tz = -(farVal+nearVal)/(farVal-nearVal);
-
-    projMatrix = M44d(xx,0.0,0.0,0.0,
-                      0.0,yy,0.0,0.0,
-                      0.0,0.0,zz,0.0,
-                      tx,ty,tz,1.0);
-}
-
-//------------------------------------------------------------------------------
 
 void drawBox(const TransformState& transState,
              const Imath::Box3d& bbox,
--- src/render/glutil.h
+++ src/render/glutil.h
@@ -23,49 +23,7 @@
 
 #include "util.h"
 #include "typespec.h"
-
-
-//-------------------------------------------------------------------------------
-struct TransformState
-{
-    Imath::V2i viewSize;
-    Imath::M44d projMatrix;
-    Imath::M44d modelViewMatrix;
-
-    TransformState(const Imath::V2i& viewSize, const M44d& projMatrix,
-                   const M44d& modelViewMatrix)
-        : viewSize(viewSize), projMatrix(projMatrix),
-        modelViewMatrix(modelViewMatrix)
-    { }
-
-    /// Return position of camera in model space
-    V3d cameraPos() const
-    {
-        return V3d(0)*modelViewMatrix.inverse();
-    }
-
-    /// Translate model by given offset
-    TransformState translate(const Imath::V3d& offset) const;
-
-    /// Scale model by given scalar
-    TransformState scale(const Imath::V3d& scalar) const;
-
-    /// Rotate model by given rotation vector
-    TransformState rotate(const Imath::V4d& rotation) const;
-
-    /// Load matrix uniforms onto the currently bound shader program:
-    ///
-    ///   "projectionMatrix"
-    ///   "modelViewMatrix"
-    ///   "modelViewProjectionMatrix"
-    ///
-    static void setUniform(GLuint prog, const char* name, const M44d& mat);
-    void setUniforms(GLuint prog) const;
-    void setProjUniform(GLuint prog) const;
-
-    void setOrthoProjection(double left, double right, double bottom, double top, double nearVal, double farVal);
-};
-
+#include "TransformState.h"
 
 //----------------------------------------------------------------------
 /// Utilites for drawing simple primitives
